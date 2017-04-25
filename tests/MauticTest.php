c4e13656e387e53d84262dd2894bba5c8048975f
John Linhart <admin@escope.cz>
2017-04-25 22:02:04 +0200

    Test get/set Mautic contact

--- tests/MauticTest.php
+++ tests/MauticTest.php
@@ -4,6 +4,7 @@ namespace Escopecz\MauticFormSubmit\Test;
 
 use Escopecz\MauticFormSubmit\Mautic;
 use Escopecz\MauticFormSubmit\Mautic\Form;
+use Escopecz\MauticFormSubmit\Mautic\Contact;
 
 class MauticTest extends \PHPUnit_Framework_TestCase
 {
@@ -23,5 +24,31 @@ class MauticTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf(Form::class, $form);
         $this->assertSame($formId, $form->getId());
+    }
+
+    function test_get_contact()
+    {
+        $mautic = new Mautic($this->baseUrl);
+        $contact = $mautic->getContact();
+
+        $this->assertInstanceOf(Contact::class, $contact);
+        $this->assertSame(0, $contact->getId());
+        $this->assertSame('', $contact->getIp());
+    }
+
+    function test_get_set_contact()
+    {
+        $mautic = new Mautic($this->baseUrl);
+        $contactId = 4;
+        $contactIp = '234.3.2.33';
+        $contactA = new Contact($contactId, $contactIp);
+        $mautic->setContact($contactA);
+        $contactB = $mautic->getContact();
+
+        $this->assertInstanceOf(Contact::class, $contactB);
+        $this->assertSame($contactA->getId(), $contactB->getId());
+        $this->assertSame($contactA->getIp(), $contactB->getIp());
+        $this->assertSame($contactId, $contactB->getId());
+        $this->assertSame($contactIp, $contactB->getIp());
     }
 }

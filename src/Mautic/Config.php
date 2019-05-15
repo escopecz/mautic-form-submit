40ff370fa21d77685a520f20832f4593072859c0
Rahul Jaisheel <9rj@users.noreply.github.com>
2019-05-15 12:31:36 +0530

    Removed constructor.

--- src/Mautic/Config.php
+++ src/Mautic/Config.php
@@ -13,16 +13,6 @@ class Config
      * @var bool
      */
     protected $curlVerbose = 1; 
-    
-    /**
-     * Constructor
-     *
-     * @param int $curlVerbose
-     */
-    public function __construct(int $curlVerbose)
-    {
-        $this->curlVerbose = $curlVerbose;
-    }
 
     /**
      * Returns Curl verbose logging option
